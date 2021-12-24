e5cf2906fc3dbcab8fb0b31376a4c3acc851caa3
MariamAmr20190520 <20190520@stud.fci-cu.edu.eg>
2021-12-24 17:33:15 +0200

    m

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]){
+        ArrayList<Process> processes = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number of processes");
+        int n = input.nextInt();
+        System.out.println("Enter context switching");
+        int contextSwitching = input.nextInt();
+        for(int i = 0; i<n; i++)
+        {
+            Scanner input2= new Scanner(System.in);
+            Process p= new Process();
+            System.out.println("Enter process "+(i+1)+" name:");
+            p.name=input2.nextLine();
+            System.out.println("Enter process "+(i+1)+" color:");
+            p.color=input2.nextLine();
+            System.out.println("Enter process "+(i+1)+" arrival time:");
+            p.arrivalTime=input.nextInt();
+            System.out.println("Enter process "+(i+1)+" burst time:");
+            p.burstTime=input2.nextInt();
+            System.out.println("Enter process "+(i+1)+" priority number:");
+            p.priorityNumber=input2.nextInt();
+            System.out.println("Enter process "+(i+1)+" time quantum: ");
+            p.quantumTime= input.nextInt();
+            processes.add(p);
+        }
+    }
+}
---
+++ src/Process.java
@@ -0,0 +1,78 @@
+public class Process {
+    public String name;
+    public String color;
+    public int arrivalTime;
+    public int burstTime;
+    public int priorityNumber;
+    public int quantumTime;
+    public int waitingTime;
+    public int turnAroundTime;
+
+    public Process() {
+    }
+
+    public Process(String name, String color, int arrivalTime, int burstTime, int priorityNumber, int quantumTime) {
+        this.name = name;
+        this.color = color;
+        this.arrivalTime = arrivalTime;
+        this.burstTime = burstTime;
+        this.priorityNumber = priorityNumber;
+        this.quantumTime = quantumTime;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setArrivalTime(int arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public void setBurstTime(int burstTime) {
+        this.burstTime = burstTime;
+    }
+
+    public void setQuantumTime(int quantumTime) {
+        this.quantumTime = quantumTime;
+    }
+
+    public void setWaitingTime(int waitingTime) {this.waitingTime = waitingTime;}
+
+    public void setturnAroundTime(int turnAroundTime) {this.turnAroundTime = turnAroundTime;}
+
+    public void setPriorityNumber(int priorityNumber) {
+        this.priorityNumber = priorityNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public int getBurstTime() {
+        return burstTime;
+    }
+
+    public int getPriorityNumber() {
+        return priorityNumber;
+    }
+
+    public int getQuantumTime() {
+        return quantumTime; }
+
+    public int getTurnAroundTime() { return turnAroundTime; }
+
+    public int getWaitingTime() { return waitingTime; }
+
+}
